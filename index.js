f7b8ce9a2bc552729bb1d8dbf905e3db92ec1f47
memoryfile <yuiotp999@protonmail.com>
2022-09-19 13:10:37 -0400

    Turn off JavaScript

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const phoneError = document.querySelector("#phone_error");
 
 // Referenced https://github.com/Appletri/sign-up-form/blob/main/index.html to study error validation via JavaScript
 
-function checkEqual() {
+// function checkEqual() {
   // if password1.getcontent = password2.getcontent -> looks good!
   // if else -> not matching
 
-  if (passwordC.value !== password.value) {
-    passwordCerror.textContent = "Passwords do not match.";
-  } else {
-    passwordCerror.textContent = "";
-  }
-}
+//   if (passwordC.value !== password.value) {
+//     passwordCerror.textContent = "Passwords do not match.";
+//   } else {
+//     passwordCerror.textContent = "";
+//   }
+// }
 
-phone.addEventListener("input", function (event) {
-  if (phone.validity.patternMismatch) {
-    phoneError.textContent = "Please enter a ten digit phone number.";
-  } else {
-    phoneError.textContent = "";
-  }
-});
+// phone.addEventListener("input", function (event) {
+//   if (phone.validity.patternMismatch) {
+//     phoneError.textContent = "Please enter a ten digit phone number.";
+//   } else {
+//     phoneError.textContent = "";
+//   }
+// });
 
 // function keyPress() {
 //   password1.addEventListener("keypress", checkEqual);
